f6d62caeb4edc7611ccba4e81585f8937a4b54bd
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-09-20 12:08:23 -0700

    Updated examples to be more like color display examples

--- examples/displayio_ssd1306_simpletest.py
+++ examples/displayio_ssd1306_simpletest.py
@@ -1,16 +1,51 @@
+"""
+This test will initialize the display using displayio and draw a solid white
+background, a smaller black rectangle, and some white text.
+"""
+
 import board
-import busio
 import displayio
+import terminalio
+from adafruit_display_text import label
 import adafruit_displayio_ssd1306
 
 displayio.release_displays()
 
-# This pinout works on a Metro and may need to be altered for other boards.
-spi = busio.SPI(board.SCL, board.SDA)
-tft_cs = board.D9
-tft_dc = board.D8
-tft_reset = board.D7
+spi = board.SPI()
+tft_cs = board.D5
+tft_dc = board.D6
+tft_reset = board.D9
 
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs,
                                  reset=tft_reset, baudrate=1000000)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+
+# Make the display context
+splash = displayio.Group(max_size=10)
+display.show(splash)
+
+color_bitmap = displayio.Bitmap(128, 64, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0xFFFFFF # White
+
+bg_sprite = displayio.TileGrid(color_bitmap,
+                               pixel_shader=color_palette,
+                               x=0, y=0)
+splash.append(bg_sprite)
+
+# Draw a smaller inner rectangle
+inner_bitmap = displayio.Bitmap(118, 54, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0x000000 # Black
+inner_sprite = displayio.TileGrid(inner_bitmap,
+                                  pixel_shader=inner_palette,
+                                  x=5, y=5)
+splash.append(inner_sprite)
+
+# Draw a label
+text = "Hello World!"
+text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=28, y=32)
+splash.append(text_area)
+
+while True:
+    pass
