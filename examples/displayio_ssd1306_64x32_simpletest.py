6512159684dd99203cd51716b8d691ce30350d6c
foamyguy <foamyguy@gmail.com>
2025-02-24 16:49:54 -0600

    remove 8x i2cdisplay api usage

--- examples/displayio_ssd1306_64x32_simpletest.py
+++ examples/displayio_ssd1306_64x32_simpletest.py
@@ -9,16 +9,9 @@ Customized version of displayio_ssd1306_simpletest.py for 64x32
 
 import board
 import displayio
-
-# Compatibility with both CircuitPython 8.x.x and 9.x.x.
-# Remove after 8.x.x is no longer a supported release.
-try:
-    from i2cdisplaybus import I2CDisplayBus
-except ImportError:
-    from displayio import I2CDisplay as I2CDisplayBus
-
 import terminalio
 from adafruit_display_text import label
+from i2cdisplaybus import I2CDisplayBus
 
 import adafruit_displayio_ssd1306
 
